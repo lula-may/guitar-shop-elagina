bb16bb5f05e0478437feefc3563ee8d97803c03b
Yulia Elagina <julia-elagina@yandex.ru>
2021-09-29 18:06:12 +0300

    Подключает компонент каталога к хранилищу, заменяет статичные данные на данные из хранилища

--- src/components/catalog/catalog.jsx
+++ src/components/catalog/catalog.jsx
@@ -1,194 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 import './style.scss';
 
+import ProductCard from '../product-card/product-card';
 import Sort from '../sort/sort';
+import { getProductsAll } from '../../store/catalog/selectors';
+
 export default function Catalog() {
+  const products = useSelector(getProductsAll);
   return (
     <section className="main__catalog catalog">
       <h2 className="visually-hidden">Список товаров</h2>
       <Sort/>
       <ul className="catalog__list">
-        <li className="catalog__item">
-          <article className="card">
-            <h3 className="visually-hidden">Карточка товара</h3>
-            <div className="card__image"><img src="img/electro.png" width="68" height="190" alt="Фото гитары" /></div>
-            <div className="card__reviews">
-              <div className="rating">
-                <span className="rating__active" style={{width: '70%'}}></span>
-              </div>
-              <span className="card__reviews-count">15</span>
-            </div>
-            <dl className="card__info">
-              <dt>Честер Bass</dt>
-              <dd>17 500 ₽</dd>
-            </dl>
-            <div className="card__footer">
-              <Link className="card__button button button--plain" to="#">Подробнее</Link>
-              <button className="card__button card__button--cart button button--bright" type="button">Купить</button>
-            </div>
-          </article>
-        </li>
-        <li className="catalog__item">
-          <article className="card">
-            <h3 className="visually-hidden">Карточка товара</h3>
-            <div className="card__image"><img src="img/classic.png" width="68" height="190" alt="Фото гитары" /></div>
-            <div className="card__reviews">
-              <div className="rating">
-                <span className="rating__active" style={{width: '70%'}}></span>
-              </div>
-              <span className="card__reviews-count">15</span>
-            </div>
-            <dl className="card__info">
-              <dt>Честер Bass</dt>
-              <dd>17 500 ₽</dd>
-            </dl>
-            <div className="card__footer">
-              <Link className="card__button button button--plain" to="#">Подробнее</Link>
-              <button className="card__button card__button--cart button button--bright" type="button">Купить</button>
-            </div>
-          </article>
-        </li>
-        <li className="catalog__item">
-          <article className="card">
-            <h3 className="visually-hidden">Карточка товара</h3>
-            <div className="card__image"><img src="img/ukulele.png" width="68" height="190" alt="Фото гитары" /></div>
-            <div className="card__reviews">
-              <div className="rating">
-                <span className="rating__active" style={{width: '70%'}}></span>
-              </div>
-              <span className="card__reviews-count">15</span>
-            </div>
-            <dl className="card__info">
-              <dt>Честер Bass</dt>
-              <dd>17 500 ₽</dd>
-            </dl>
-            <div className="card__footer">
-              <Link className="card__button button button--plain" to="#">Подробнее</Link>
-              <button className="card__button card__button--cart button button--bright" type="button">Купить</button>
-            </div>
-          </article>
-        </li>
-        <li className="catalog__item">
-          <article className="card">
-            <h3 className="visually-hidden">Карточка товара</h3>
-            <div className="card__image"><img src="img/electro.png" width="68" height="190" alt="Фото гитары" /></div>
-            <div className="card__reviews">
-              <div className="rating">
-                <span className="rating__active" style={{width: '70%'}}></span>
-              </div>
-              <span className="card__reviews-count">15</span>
-            </div>
-            <dl className="card__info">
-              <dt>Честер Bass</dt>
-              <dd>17 500 ₽</dd>
-            </dl>
-            <div className="card__footer">
-              <Link className="card__button button button--plain" to="#">Подробнее</Link>
-              <button className="card__button card__button--cart button button--bright" type="button">Купить</button>
-            </div>
-          </article>
-        </li>
-        <li className="catalog__item">
-          <article className="card">
-            <h3 className="visually-hidden">Карточка товара</h3>
-            <div className="card__image"><img src="img/classic.png" width="68" height="190" alt="Фото гитары" /></div>
-            <div className="card__reviews">
-              <div className="rating">
-                <span className="rating__active" style={{width: '90%'}}></span>
-              </div>
-              <span className="card__reviews-count">15</span>
-            </div>
-            <dl className="card__info">
-              <dt>Честер Bass</dt>
-              <dd>17 500 ₽</dd>
-            </dl>
-            <div className="card__footer">
-              <Link className="card__button button button--plain" to="#">Подробнее</Link>
-              <button className="card__button card__button--cart button button--bright" type="button">Купить</button>
-            </div>
-          </article>
-        </li>
-        <li className="catalog__item">
-          <article className="card">
-            <h3 className="visually-hidden">Карточка товара</h3>
-            <div className="card__image"><img src="img/ukulele.png" width="68" height="190" alt="Фото гитары" /></div>
-            <div className="card__reviews">
-              <div className="rating">
-                <span className="rating__active" style={{width: '100%'}}></span>
-              </div>
-              <span className="card__reviews-count">15</span>
-            </div>
-            <dl className="card__info">
-              <dt>Честер Bass</dt>
-              <dd>17 500 ₽</dd>
-            </dl>
-            <div className="card__footer">
-              <Link className="card__button button button--plain" to="#">Подробнее</Link>
-              <button className="card__button card__button--cart button button--bright" type="button">Купить</button>
-            </div>
-          </article>
-        </li>
-        <li className="catalog__item">
-          <article className="card">
-            <h3 className="visually-hidden">Карточка товара</h3>
-            <div className="card__image"><img src="img/electro.png" width="68" height="190" alt="Фото гитары" /></div>
-            <div className="card__reviews">
-              <div className="rating">
-                <span className="rating__active" style={{width: '70%'}}></span>
-              </div>
-              <span className="card__reviews-count">15</span>
-            </div>
-            <dl className="card__info">
-              <dt>Честер Bass</dt>
-              <dd>17 500 ₽</dd>
-            </dl>
-            <div className="card__footer">
-              <Link className="card__button button button--plain" to="#">Подробнее</Link>
-              <button className="card__button card__button--cart button button--bright" type="button">Купить</button>
-            </div>
-          </article>
-        </li>
-        <li className="catalog__item">
-          <article className="card">
-            <h3 className="visually-hidden">Карточка товара</h3>
-            <div className="card__image"><img src="img/classic.png" width="68" height="190" alt="Фото гитары" /></div>
-            <div className="card__reviews">
-              <div className="rating">
-                <span className="rating__active" style={{width: '90%'}}></span>
-              </div>
-              <span className="card__reviews-count">15</span>
-            </div>
-            <dl className="card__info">
-              <dt>Честер Bass</dt>
-              <dd>17 500 ₽</dd>
-            </dl>
-            <div className="card__footer">
-              <Link className="card__button button button--plain" to="#">Подробнее</Link>
-              <button className="card__button card__button--cart button button--bright" type="button">Купить</button>
-            </div>
-          </article>
-        </li>
-        <li className="catalog__item">
-          <article className="card">
-            <h3 className="visually-hidden">Карточка товара</h3>
-            <div className="card__image"><img src="img/ukulele.png" width="68" height="190" alt="Фото гитары" /></div>
-            <div className="card__reviews">
-              <div className="rating">
-                <span className="rating__active" style={{width: '100%'}}></span>
-              </div>
-              <span className="card__reviews-count">15</span>
-            </div>
-            <dl className="card__info">
-              <dt>Честер Bass</dt>
-              <dd>17 500 ₽</dd>
-            </dl>
-            <div className="card__footer">
-              <Link className="card__button button button--plain" to="#">Подробнее</Link>
-              <button className="card__button card__button--cart button button--bright" type="button">Купить</button>
-            </div>
-          </article>
-        </li>
+        {products.map((product) => (
+          <li key={product.id} className="catalog__item">
+            <ProductCard product={product} />
+          </li>
+        ))}
       </ul>
       <div className="catalog__pagination">
         <div className="pagination">
@@ -203,3 +34,4 @@ export default function Catalog() {
     </section>
   );
 }
+
