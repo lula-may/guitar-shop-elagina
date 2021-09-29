6066f7a428b15868e1354906ab2c90a05f502b92
Yulia Elagina <julia-elagina@yandex.ru>
2021-09-29 13:56:18 +0300

    Подключает хранилище в index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+import './style.scss';
 import App from './components/app/app';
-import "./style.scss";
+import { products } from './mocks/data';
+import { rootReducer } from './store/root-reducer';
+import { setProducts } from './store/actions';
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+store.dispatch(setProducts(products));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'));
 
