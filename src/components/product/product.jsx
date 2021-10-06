a52b916a3b053c2b208c148789fdb18ee80bce80
Yulia Elagina <julia-elagina@yandex.ru>
2021-10-06 16:37:31 +0300

    Добавляет возможность редактирования кол-ва товаров в корзине в поле input

--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import './style.scss';
@@ -7,10 +7,22 @@ import { PRODUCT } from '../props';
 import { GuitarOption, PopupType } from '../../const';
 import { setPopup, setPopupProduct, updateCartProduct } from '../../store/actions';
 
+const SYMBOL_WIDTH = 10;
+
+const getInputStyle = (counter) => {
+  const counterLength = counter.toString().length;
+  if (counterLength < 3) {
+    return null;
+  }
+  const inputWidth = SYMBOL_WIDTH * counterLength + 6;
+  return {width: `${inputWidth}px`};
+};
+
 export default function Product({counter, product}) {
   const {article, id, name, type, strings, price} = product;
   const {previews, type: text} = GuitarOption[type];
   const [preview, preview2x] = previews;
+  const [currentCounter, setCurrentCounter]= useState(counter);
   const totalPrice = price * counter;
   const dispatch = useDispatch();
 
@@ -21,23 +33,42 @@ export default function Product({counter, product}) {
 
   const handlePlusButtonClick = useCallback(() => {
     dispatch(updateCartProduct({product, counter: counter + 1}));
+    setCurrentCounter((prevCounter) => prevCounter + 1);
   }, [counter, dispatch, product]);
 
   const handleMinusButtonClick = useCallback(() => {
     if (counter > 1) {
       dispatch(updateCartProduct({product, counter: counter - 1}));
+      setCurrentCounter((prevCounter) => prevCounter - 1);
     } else {
       openPopup();
     }
   }, [counter, dispatch, openPopup, product]);
 
   const handleCountInputChange = useCallback(({target}) => {
+    if (target.value === '') {
+      setCurrentCounter('');
+      return;
+    }
     const newValue = Number(target.value);
     if (isNaN(newValue)) {
       return;
     }
+    if (newValue === 0) {
+      openPopup();
+      setCurrentCounter(counter);
+      return;
+    }
     dispatch(updateCartProduct({product, counter: newValue}));
-  }, [dispatch, product]);
+    setCurrentCounter(newValue);
+  }, [counter, dispatch, openPopup, product]);
+
+  const handleInputBlur = useCallback(({target}) => {
+    if (target.value === '') {
+      openPopup();
+      setCurrentCounter(counter);
+    }
+  }, [counter, openPopup]);
 
   const handleDeleteButtonClick = useCallback(() => openPopup(), [openPopup]);
 
@@ -71,8 +102,10 @@ export default function Product({counter, product}) {
             type="text"
             name={`counter-${id}`}
             id={`counter-${id}`}
-            value={counter}
+            value={currentCounter}
             onChange={handleCountInputChange}
+            onBlur={handleInputBlur}
+            style={getInputStyle(currentCounter)}
           />
           <button
             className="product__control product__control--plus"
